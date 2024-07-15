e84bd55c6eeb0b2f49a8f5fea734dc9fc98dbfd3
mxclee <mxclee@ucdavis.edu>
2024-07-15 13:17:20 -0700

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -20,7 +20,7 @@ btn_guess = st.button('Make Guess')
 
 if btn_guess:
 
-    if txt_guess == num or txt_guess == num1
+    if txt_guess == num and txt_guess == num1:
 
         st.write('You Win')
 
