b7b57394bf59f776e13f78d951044e240e93996a
mxclee <mxclee@ucdavis.edu>
2024-07-15 13:11:05 -0700

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -8,49 +8,36 @@ st.write('### where you guess a number ')
 
  
 
-num = random.randrange(1, 4)
-
- 
+num = random.randrange(1, 4) 
 
 txt_guess = int(st.text_input('Enter a number between 1 and 4: ', 1))
 
- 
+num1 = random.randrange(5, 8)
+
+txt_guess = int(st.text_input('Enter a number between 5 and 7: ', 5))
 
 btn_start = st.button('Start Again')
 
- 
-
 btn_guess = st.button('Make Guess')
-
- 
 
 if btn_guess:
 
-    if txt_guess == num:
+    if txt_guess == num or txt_guess == num1:
 
         st.write('You Win')
 
         st.balloons()
 
     else:
-
-       
-
         html_str = f""" <h1 style='text-align: left; color: #FF4433;'> Sorry . Try again. </h1> """
 
         st.markdown(html_str, unsafe_allow_html=True)
 
- 
-
 btn_show = st.button('Show Number')
-
- 
 
 if btn_show:
 
     st.write('The number is ', num)
-
- 
 
 with st.expander("Help..."):
 
