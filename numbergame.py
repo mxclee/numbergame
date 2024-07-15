7be80bcae865ed16eeff426100ffa5aeb3624892
mxclee <mxclee@ucdavis.edu>
2024-07-15 13:24:15 -0700

    pt2

--- numbergame.py
+++ numbergame.py
@@ -27,7 +27,7 @@ if btn_guess:
         st.balloons()
 
     else:
-        html_str = f""" <h1 style='text-align: left; color: #FF4433;'> Sorry . Try again. </h1> """
+        html_str = f""" <h1 style='text-align: left; color: #FF4433;'> Sorry. Try again. </h1> """
 
         st.markdown(html_str, unsafe_allow_html=True)
 
