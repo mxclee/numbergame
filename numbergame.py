51c1d903fe3d241ac77064b35aaba2982a68ab9d
mxclee <mxclee@ucdavis.edu>
2024-07-15 14:17:04 -0700

    d

--- numbergame.py
+++ numbergame.py
@@ -10,31 +10,35 @@ st.write('Choose either 1-4 or 5-8')
 
 btn_one = st.button('1-4')
 btn_two = st.button('5-8')
-btn_guess = st.button('guess')
+btn_guess = st.button('Make Guess')
+
 if btn_one:
     num = random.randrange(1, 4)
     txt_guess = int(st.text_input('Enter a number between 1 and 4: ', 1))
+    if btn_guess:
+         if txt_guess == num:
+
+            st.write('You Win')
+            st.balloons()
+         else:
+            html_str = f""" <h1 style='text-align: left; color: #FF4433;'> Sorry. Try again. </h1> """
+
+            st.markdown(html_str, unsafe_allow_html=True)
 
 else:
     num1 = random.randrange(5, 8)
     txt_guess1 = int(st.text_input('Enter a number between 5 and 8: ', 5))
-    
+    if btn_guess:
+         if txt_guess1 == num1:
+             st.write('You Win')
+             st.balloons()
+         else:
+             html_str = f""" <h1 style='text-align: left; color: #FF4433;'> Sorry. Try again. </h1> """
+             st.markdown(html_str, unsafe_allow_html=True)
 
 btn_start = st.button('Start Again')
 
-btn_guess = st.button('Make Guess')
 
-if btn_guess:
-
-    if txt_guess == num or txt_guess1 == num1:
-
-        st.write('You Win')
-        st.balloons()
-    
-    else:
-        html_str = f""" <h1 style='text-align: left; color: #FF4433;'> Sorry. Try again. </h1> """
-
-        st.markdown(html_str, unsafe_allow_html=True)
 
 st.write('The number is', num, 'or', num1)
 
