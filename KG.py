30f015b67a9c342b9f7bceb730d180f5d0cba93d
mxclee <mxclee@ucdavis.edu>
2024-07-16 14:01:37 -0700

    Update KG.py

--- KG.py
+++ KG.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_graph import agraph, Node, Edge, Config
+from streamlit_agraph import agraph, Node, Edge, Config
 import pandas as pd
 st.set_page_config(layout="wide")
 from streamlit_agraph.config import Config, ConfigBuilder
