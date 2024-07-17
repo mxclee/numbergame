b3fa02dcb93063ca1911c70e09b4e054cadceae7
mxclee <mxclee@ucdavis.edu>
2024-07-17 09:51:32 -0700

    Update KG.py

--- KG.py
+++ KG.py
@@ -14,31 +14,7 @@ if Disease:
      option = st.selectbox(
     'Choose disease type:',
     ('ARR', 'CHD', 'CM', 'CVA', 'IHD', 'VD'))
-else:
-      option = st.selectbox(
-        'Choose CoMorbidity type:',
-        ('Heart failure', 'Liver dysfunction', 'Lung dysfunction', 'Cancer', 'Liver fibrosis', 'Kidney dysfunction'))
-      
-final_arr_short = final_vd[final_vd.Condition == option]
-
-# initializing buckets
-nodes = []
-edges = []
-
-df_genes = dict()
-
-st.title('Knowledge Graph')
-df_genes=dict(enumerate(final_arr_short.Protein.unique()))
-for i in df_genes:
-  nodes.append(Node(id=df_genes[i],
-                    label=df_genes[i],
-                    size=25,
-                    shape="diamond",
-                    color='#00008B'
-                   )
-              )
-
-df_disease = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
+  df_disease = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
 df_disease = df_disease.sort_index(axis = 0, ascending=True)
 df_disease = df_disease[df_disease.name != 'na']
 for index, row in df_disease.iterrows():
@@ -49,7 +25,39 @@ for index, row in df_disease.iterrows():
                         color='#bf9b30'
                         )
                     ) # includes **kwargs
-df_comorbidities = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
+  df_condition = dict()
+df_condition=dict(enumerate(final_arr_short.Condition.unique()))
+for k in df_condition:
+            nodes.append( Node(id=df_condition[k],
+                        label=f"          {option}          ",
+                        size=200,
+                        shape="circle",
+                        color='#00FFFF'
+                        )
+                    ) # includes **kwargs
+
+df_connections = final_arr_short.filter(items=['Protein','neighbour_name']).drop_duplicates()
+df_connections = df_connections[df_connections.neighbour_name != 'na']
+for index, row in df_connections.iterrows(): 
+      edges.append(Edge(source=row['Protein'],
+                      label="--",
+                      target=row['neighbour_name'],
+                      #**kwargs
+                      )
+                  )
+df_mconnections = final_arr_short.filter(items=['Protein','Condition']).drop_duplicates()
+for index, row in df_mconnections.iterrows():
+      edges.append(Edge(source=row['Condition'],
+                      label="--",
+                      target=row['Protein'],
+                      #**kwargs
+                      )
+                  )
+else: 
+      option = st.selectbox(
+        'Choose CoMorbidity type:',
+        ('Heart failure', 'Liver dysfunction', 'Lung dysfunction', 'Cancer', 'Liver fibrosis', 'Kidney dysfunction'))
+  df_comorbidities = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
 df_comorbidities = df_comorbidities.sort_index(axis = 0, ascending=True)
 df_comorbidities = df_comorbidities[df_comorbidities.name != 'na']
 for index, row in df_comorbidities.iterrows():
@@ -88,6 +96,26 @@ for index, row in df_mconnections.iterrows():
                       #**kwargs
                       )
                   )
+      
+final_arr_short = final_vd[final_vd.Condition == option]
+
+# initializing buckets
+nodes = []
+edges = []
+
+df_genes = dict()
+
+st.title('Knowledge Graph')
+df_genes=dict(enumerate(final_arr_short.Protein.unique()))
+for i in df_genes:
+  nodes.append(Node(id=df_genes[i],
+                    label=df_genes[i],
+                    size=25,
+                    shape="diamond",
+                    color='#00008B'
+                   )
+              )
+  
 config_builder = ConfigBuilder(nodes)
 config = config_builder.build()
 
