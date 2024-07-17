2c32fbb5ef929636b887dee9b69d812197d34111
mxclee <mxclee@ucdavis.edu>
2024-07-17 09:11:58 -0700

    Update KG.py

--- KG.py
+++ KG.py
@@ -9,9 +9,11 @@ final_vd = pd.read_csv(r'./final_top5.csv')
 with st.sidebar:
   option = st.selectbox(
     'Please select your type:',
-    ('CVA', 'IHD', 'CM', 'ARR', 'VD', 'CHD'))
+    ('CVA','IHD','CM','ARR','VD','CHD'))
+
 final_arr_short = final_vd[final_vd.Condition == option]
 
+# initializing buckets
 nodes = []
 edges = []
 
@@ -20,14 +22,14 @@ df_genes = dict()
 st.title('Knowledge Graph')
 df_genes=dict(enumerate(final_arr_short.Protein.unique()))
 for i in df_genes:
-  nodes.append( Node(id=df_genes[i],
+  nodes.append(Node(id=df_genes[i],
                      label=df_genes[i],
                      size=25,
                      shape="diamond",
                      color='#00008B'
                     )
-              )
-df_disease = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
+              ) # includes **kwargs
+df_disease = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name","count"])
 df_disease = df_disease.sort_index(axis = 0, ascending=True)
 df_disease = df_disease[df_disease.name != 'na']
 for index, row in df_disease.iterrows():
@@ -37,32 +39,33 @@ for index, row in df_disease.iterrows():
                      shape="square",
                      color='#bf9b30'
                     )
-              )
+              ) # includes **kwargs
   df_condition = dict()
   df_condition=dict(enumerate(final_arr_short.Condition.unique()))
   for k in df_condition:
-    nodes.append( Node(id=df_condition[k],
-                       label=f"    {option} ",
+    nodes.append(Node(id=df_condition[k],
+                       label=f"{option}",
                        size=200,
                        shape="circle",
                        color='#00FFFF'
                        )
-                 ) 
+                 ) # includes **kwargs
 
-df_connections = final_arr_short.filter(items=['Protein', 'neighbour_name']).drop_duplicates()
+df_connections = final_arr_short.filter(items=['Protein','neighbour_name']).drop_duplicates()
 df_connections = df_connections[df_connections.neighbour_name != 'na']
-
-for index, row in df_connections.iterrows(): #problem here too
-      edges.append( Edge(source=row['Protein'],
+for index, row in df_connections.iterrows(): 
+      edges.append(Edge(source=row['Protein'],
                       label="--",
                       target=row['neighbour_name'],
+                      #**kwargs
                       )
                   )
-df_mconnections = final_arr_short.filter(items=['Protein', 'Condition']).drop_duplicates()
+df_mconnections = final_arr_short.filter(items=['Protein','Condition']).drop_duplicates()
 for index, row in df_mconnections.iterrows():
-      edges.append( Edge(source=row['Condition'],
+      edges.append(Edge(source=row['Condition'],
                       label="--",
                       target=row['Protein'],
+                      #**kwargs
                       )
                   )
 config_builder = ConfigBuilder(nodes)
