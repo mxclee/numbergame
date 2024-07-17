813c62c10d97eabd84306202028463f5416bacfa
mxclee <mxclee@ucdavis.edu>
2024-07-17 10:51:40 -0700

    Update KG.py

--- KG.py
+++ KG.py
@@ -23,6 +23,12 @@ if option:
 
 final_arr_short = final_arr_short1[final_arr_short1.Condition == option2]
 
+final_arr_short = final_vd[final_vd.Condition == option]
+res = final_vd['Condition'].value_counts()\
+                .to_frame('Disease_count').rename_axis('Condition')\
+                .reset_index()
+st.bar_chart(res,x="Condition", y="Disease_count")
+
 # initializing buckets
 nodes = []
 edges = []
