e65b8d6aed95fa8dc66d741b75f6156108737070
mxclee <mxclee@ucdavis.edu>
2024-07-17 10:13:19 -0700

    Update KG.py

--- KG.py
+++ KG.py
@@ -10,7 +10,25 @@ with st.sidebar:
   option = st.selectbox(
     'Please select Disease or CoMordities:',
     ('Disease', 'CoMorbidities'))
-  
+
+final_arr_short = final_vd[final_vd.Condition == option] 
+# initializing buckets
+nodes = []
+edges = []
+
+df_genes = dict()
+
+st.title('Knowledge Graph')
+df_genes=dict(enumerate(final_arr_short.Protein.unique()))
+for i in df_genes:
+  nodes.append(Node(id=df_genes[i],
+                    label=df_genes[i],
+                    size=25,
+                    shape="diamond",
+                    color='#00008B'
+                   )
+              )
+
 if option == 'Disease':
      option = st.selectbox(
     'Choose disease type:',
@@ -98,25 +116,6 @@ for index, row in df_mconnections.iterrows():
                       )
                   )
       
-final_arr_short = final_vd[final_vd.Condition == option]
-
-# initializing buckets
-nodes = []
-edges = []
-
-df_genes = dict()
-
-st.title('Knowledge Graph')
-df_genes=dict(enumerate(final_arr_short.Protein.unique()))
-for i in df_genes:
-  nodes.append(Node(id=df_genes[i],
-                    label=df_genes[i],
-                    size=25,
-                    shape="diamond",
-                    color='#00008B'
-                   )
-              )
-  
 config_builder = ConfigBuilder(nodes)
 config = config_builder.build()
 
