408b850eaf415b4218aad45208c77209601c057b
mxclee <mxclee@ucdavis.edu>
2024-07-17 11:21:06 -0700

    Update KG.py

--- KG.py
+++ KG.py
@@ -24,7 +24,7 @@ if option:
 final_arr_short = final_arr_short1[final_arr_short1.Condition == option2]
 
 final_arr_short2 = final_vd[final_vd.Condition == option]
-res = final_vd['Condition'].value_counts()\
+res = final_arr_short1['Condition'].value_counts()\
                 .to_frame('Disease_count').rename_axis('Condition')\
                 .reset_index()
 st.bar_chart(res,x="Condition", y="Disease_count")
