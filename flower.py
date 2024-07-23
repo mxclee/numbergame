d042ed67df3d3b7f22c3580717503726664e334f
mxclee <mxclee@ucdavis.edu>
2024-07-23 14:05:27 -0700

    Update flower.py

--- flower.py
+++ flower.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 
 import matplotlib.pyplot as plt
-from sklearn import datasets
+from sklearn import datasets 
 from sklearn.model_selection import train_test_split
 
 from sklearn.decomposition import PCA
